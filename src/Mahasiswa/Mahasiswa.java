d7c250d2a4386c6005d6eabc5384deebc99ef708
asus <asus@LAPTOP-BD2P4KVT>
2020-11-04 20:51:33 +0700

    new commit

---
+++ src/Mahasiswa/Mahasiswa.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mahasiswa;
+
+/**
+ *
+ @author Megianto Adi saputra
+ * Nama         : Megianto Adi Saputra
+ * Kelas        : IF-2
+ * NIM          : 10119060
+ * Deskripsi    : Program Perkenalan Mahasiswa
+ */   
+public class Mahasiswa {
+    public String nim;
+    public String nama;
+    
+    public void perkenalanDiri(){
+        System.out.println("Hallo Everyone");
+        System.out.println("My NIM is " + this.nim);
+        System.out.println("My NIM is " + this.nama);
+        System.out.println("");
+}
+    
+}
---
+++ src/pboif2/pkg10119060/latihan31/perkenalanmahasiswa/PBOIF210119060Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119060.latihan31.perkenalanmahasiswa;
+import Mahasiswa.Mahasiswa;
+/**
+ *
+ @author Megianto Adi saputra
+ * Nama         : Megianto Adi Saputra
+ * Kelas        : IF-2
+ * NIM          : 10119060
+ * Deskripsi    : Program Perkenalan mahasiswa
+ */
+
+public class PBOIF210119060Latihan31PerkenalanMahasiswa {
+
+    public static void main(String[] args) {
+       Mahasiswa mahasiswa1 = new Mahasiswa();
+       mahasiswa1.nim = "10119060";
+       mahasiswa1.nama = "Megianto Adi Saputra";
+       mahasiswa1.perkenalanDiri();
+       
+       Mahasiswa mahasiswa2 = new Mahasiswa();
+       mahasiswa2.nim = "10110270";
+       mahasiswa2.nama = "Indra Tiola";
+       mahasiswa2.perkenalanDiri();
+       
+       Mahasiswa mahasiswa3 = new Mahasiswa();
+       mahasiswa3.nim = "10110271";
+       mahasiswa3.nama = "Robi Tanzil Ganefi";
+       mahasiswa3.perkenalanDiri();
+       
+       Mahasiswa mahasiswa4 = new Mahasiswa();
+       mahasiswa4.nim = "1011069";
+       mahasiswa4.nama = "Muhammad Nur Awaludin";
+       mahasiswa1.perkenalanDiri();
+      
+     
+    }
+        
+               
+  }
+    
